4d97d0cb3193b28baf0718be08a858ad7947c981
OlegSolovyev <olegius.solo@gmail.com>
2020-08-11 12:50:47 +0200

    "Calc added"

---
+++ Calc/src/com/company/Calc.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Calc {
+    private int a;
+    private int b;
+
+    public Calc(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int plus(){
+        return a+b;
+    }
+
+    public int minus(){
+        return a-b;
+    }
+    public int multiply(){
+        return a*b;
+    }
+
+    public int divide(){
+        return a/b;
+    }
+}
